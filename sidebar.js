3bca3306d2d3c0c7ccde594267e3f950437e87a4
mikehinford <mikehinford@hotmail.com>
2026-09-14 23:00:35 +0100

    update news titles and UK AQ logo

--- sidebar.js
+++ sidebar.js
@@ -764,7 +764,12 @@
 
     // Shared top-right UK AQ home logo
     const homeLogo = document.createElement('a');
-    const twoLineMobileLogoPages = new Set(['sensor-map', 'resources', 'contact']);
+	const twoLineMobileLogoPages = new Set([
+	  'sensor-map',
+	  'resources',
+	  'contact',
+	  'news',
+	]);
     const mobileHomeLogoSrc = document.body.classList.contains('hex-map-page')
       || twoLineMobileLogoPages.has(document.body.dataset.pageSlug)
       ? '/sidebar-images/UK-AQ-Logo-v3-2Lines.svg'
